82c34cb6d49374206611b9c961659b4273699b76
Gabriel Souza <47895439+GabrielSouzaHub@users.noreply.github.com>
2021-11-24 11:52:02 -0300

    Delete Scripts directory

--- Scripts/fornecedor.sql
+++
@@ -1,8 +0,0 @@
-create table fornecedor (
-	id_fornecedor INT PRIMARY KEY,
-	nome VARCHAR(50)
-);
-
-insert into fornecedor (id_fornecedor, nome) values (1, 'Steffie Philps');
-insert into fornecedor (id_fornecedor, nome) values (2, 'Pammi Haynesford');
-insert into fornecedor (id_fornecedor, nome) values (3, 'Raddy MacPeice');
